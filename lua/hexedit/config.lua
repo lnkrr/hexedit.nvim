ae9dc3f02e140ad619977df614243dda4c279221
lnkrr <lnkrr@proton.me>
2026-05-11 17:18:09 +0300

    feat: add opts.xxd.cols

--- lua/hexedit/config.lua
+++ lua/hexedit/config.lua
@@ -3,17 +3,19 @@ local M = {}
 M.opts = {
     encode = function(data)
         local result = vim.system(
-            { "xxd", "-g" .. M.opts.xxd.groupsize },
+            { "xxd", "-g" .. M.opts.xxd.groupsize, "-c" .. M.opts.xxd.cols },
             { stdin = data, text = false }
         ):wait()
 
         return result.stdout
     end,
     decode = function(data)
-        local result = vim.system(
-            { "xxd", "-r", "-g" .. M.opts.xxd.groupsize },
-            { stdin = data }
-        ):wait()
+        local result = vim.system({
+            "xxd",
+            "-r",
+            "-g" .. M.opts.xxd.groupsize,
+            "-c" .. M.opts.xxd.cols,
+        }, { stdin = data }):wait()
 
         return result.stdout
     end,
@@ -21,26 +23,29 @@ M.opts = {
         to_encoded = function(offset)
             local step = 2 * M.opts.xxd.groupsize + 1
 
-            local line = math.floor(offset / 16) + 1
-            local column = 11 + offset % 16 * step
+            local line = math.floor(offset / M.opts.xxd.cols) + 1
+            local column = 11 + offset % M.opts.xxd.cols * step
 
             return line, column
         end,
         to_decoded = function(line, column)
             local step = 2 * M.opts.xxd.groupsize + 1
-            return (line - 1) * 16 + math.floor((column - 11) / step) + 1
+            return (line - 1) * M.opts.xxd.cols
+                + math.floor((column - 11) / step)
+                + 1
         end,
         snap = function(line, column)
             local step = 2 * M.opts.xxd.groupsize + 1
             return line,
                 math.min(
                     math.max(column, 11),
-                    9 + math.ceil(step * 16 / M.opts.xxd.groupsize)
+                    9 + math.ceil(step * M.opts.xxd.cols / M.opts.xxd.groupsize)
                 )
         end,
     },
     xxd = {
         groupsize = 1,
+        cols = 20,
     },
     filetype = "xxd",
 }
